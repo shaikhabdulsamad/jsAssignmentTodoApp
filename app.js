71f8822008e4397b313c102795eccc1d085a271a
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-06-26 13:54:36 +0500

    style edited functionality edited updated

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
 
-
-
 function addList(){
     var input = document.getElementById('input')
    var ul = document.getElementById('ul')
    var li = document.createElement('li');
-//    var listInput = document.createElement('textarea');
-//    listInput.setAttribute("row", "10")
+
    var text = document.createTextNode(input.value)
-//    listInput.value = input.value
-   
-    
-    var btndiv = document.createElement('div')
-    var textdiv = document.createElement('div')
-    textdiv.className = "textdiv"
-    btndiv.className = "btndiv"
-    var btn1 = document.createElement('button')
-    var btn2 = document.createElement('button')
+
+
+    var listInput = document.createElement('input')
+    listInput.setAttribute("disabled","disabled")
+    listInput.setAttribute("class","listInput")
+    listInput.value = text.nodeValue
+    var span1 = document.createElement('span')
+    span1.setAttribute("onclick","edit(this)")
+    var span2 = document.createElement('span')
+    span2.setAttribute("onclick","delList(this)")
     var img1 = document.createElement('img')
     var img2 = document.createElement('img')
     img1.src = 'icons/edit-regular.svg'
     img2.src = 'icons/trash-solid.svg'
+    span1.appendChild(img1)
+    span2.appendChild(img2)
 
-textdiv.appendChild(text)
-    li.appendChild(textdiv)
-    btn1.appendChild(img1)
-    btn2.appendChild(img2)
 
-    btndiv.appendChild(btn1)
-    btndiv.appendChild(btn2)
-    li.appendChild(btndiv)
-    console.log(ul)
+    li.appendChild(listInput)
+    li.appendChild(span1)
+    li.appendChild(span2)
 
     
 
@@ -38,4 +33,38 @@ ul.appendChild(li)
 
 input.value = ""
 
+}
+
+
+
+
+var startstop = 0;
+
+function edit(e){
+
+    startstop = startstop + 1;
+
+    if (startstop === 1) {
+        
+        e.firstChild.src = "icons/share-square-regular.svg"
+        e.parentNode.firstChild.disabled = false
+        e.parentNode.firstChild.style.borderBottom = "2px solid red"
+
+    } else if (startstop === 2) {
+        e.firstChild.src = "icons/edit-regular.svg"
+        e.parentNode.firstChild.disabled = true
+        e.parentNode.firstChild.style.borderBottom = "none"
+        startstop = 0;
+        
+    }
+
+}
+    
+
+function delList(e){
+e.parentNode.remove()
+}
+
+function empty(){
+    document.getElementById('ul').innerHTML = ""
 }
